83c81120b072d290f72beb93d01ef4e2e4e578d8
Adam Siekierski <adam.siekiera@outlook.com>
2020-07-03 12:34:49 +0200

    express.js client paths and redirects

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 function forFramework(framework, handlers) {
-  handlers[framework]();
+  return handlers[framework];
 }
 
 /**
@@ -10,16 +10,38 @@ function forFramework(framework, handlers) {
  * @param {object} config - server client configuration of gatsby-plugin-nodejs
  * @param {function} cb - callback with rest of app logic inside
  */
-function prepare({ app, framework = "express", pathPrefix = "/" }, cb) {
+function prepare({ app, framework = "express" }, cb) {
   // Serve static Gatsby files
   forFramework(framework, {
     express: () => {
       const express = require("express");
       app.use(pathPrefix, express.static("public"));
     },
-  });
+  })();
 
   const config = JSON.parse(fs.readFileSync(path.resolve("./public", "gatsby-plugin-node.json")));
+
+  // Gatsby redirects
+  for (const r of config.redirects) {
+    forFramework(framework, {
+      express: () => {
+        app.get(r.fromPath, (req, res) =>
+          res.status(r.statusCode || r.isPermanent ? 301 : 302).redirect(r.toPath),
+        );
+      },
+    })();
+  }
+
+  // Client paths
+  for (const p of config.paths.filter((p) => p.matchPath)) {
+    forFramework(framework, {
+      express: () => {
+        app.get(p.matchPath, (req, res) =>
+          res.sendFile(path.resolve("./public", p.path, "index.html")),
+        );
+      },
+    });
+  }
 
   // User-defined routes
   cb();
@@ -31,7 +53,7 @@ function prepare({ app, framework = "express", pathPrefix = "/" }, cb) {
         res.sendFile(path.resolve("./public", "404.html"));
       });
     },
-  });
+  })();
 }
 
 module.exports = {
